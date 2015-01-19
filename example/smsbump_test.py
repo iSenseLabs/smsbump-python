f5dfff6aac7bcd18291e89389738fd7735ecc8ad
Ivailo Hristov <ivailo.hristov@isenselabs.com>
2015-01-19 10:51:29 +0200

    Remove extra bracket

--- example/smsbump_test.py
+++ example/smsbump_test.py
@@ -5,6 +5,6 @@ def printResponse(response):
     print response
 
 
-smsbump.send(apikey='{your_api_key}}', toNumber=['{recipient_number}}',
-             '{recipient_number}}'], message='Hello from SmsBump!',
+smsbump.send(apikey='{your_api_key}', toNumber=['{recipient_number}',
+             '{recipient_number}'], message='Hello from SmsBump!',
              callback=printResponse)
